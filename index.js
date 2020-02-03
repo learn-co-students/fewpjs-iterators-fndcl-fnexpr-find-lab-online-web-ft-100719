0cc6fd639a09ba9696370fb6fa4608f96e147a7b
Huda Yousif <hjoski@gmail.com>
2020-02-02 23:28:06 -0800

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let team = array.find(obj => obj.result === 'W')
+  return team ? team.year : undefined
+}
