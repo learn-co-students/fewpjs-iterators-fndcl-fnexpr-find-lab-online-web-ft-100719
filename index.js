0b46132f6389ac1a1099985e88af261b0604a490
Jesse <jesse.lee.smith@gmail.com>
2020-02-06 12:11:45 -0800

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arrayOfObjects) {
+  const winYear = arrayOfObjects.find(obj => {
+    return obj.result === 'W'
+  })
+  if (winYear) {
+    return winYear.year
+  } else {
+    return undefined
+  }
+}
